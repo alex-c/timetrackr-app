8ff456394e8db5893b772bcc59bbb1e499040cd9
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-04-01 15:42:48 +0200

    Implemented basic time tracking

--- timetrackr/lib/main.dart
+++ timetrackr/lib/main.dart
@@ -1,41 +1,23 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Timetrackr());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Timetrackr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Timetrackr',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Timetrackr'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +26,98 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool _tracking = false;
+  DateTime _startTime;
+  Duration _currentDuration;
+  Duration _trackedDuration;
+  List<Duration> _history = List<Duration>();
 
-  void _incrementCounter() {
+  String _timeDisplay() {
+    if (_tracking) {
+      return _formatDuration(_currentDuration);
+    } else {
+      if (_trackedDuration == null) {
+        return "Start tracking time!";
+      } else {
+        return _formatDuration(_trackedDuration);
+      }
+    }
+  }
+
+  void _toggleTracking() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _tracking = !_tracking;
     });
+    if (_tracking) {
+      _startTime = DateTime.now();
+    } else {
+      _trackedDuration = _currentDuration;
+      _history.add(_trackedDuration);
+    }
+  }
+
+  void _updateCurrentDuration() {
+    if (_tracking && _startTime != null) {
+      setState(() {
+        _currentDuration = DateTime.now().difference(_startTime);
+      });
+    }
+  }
+
+  String _formatDuration(Duration duration) {
+    return duration.toString().substring(0, 7);
+  }
+
+  @override
+  void initState() {
+    _currentDuration = Duration();
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _updateCurrentDuration());
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              height: 120,
+              child: Center(
+                child: Text(
+                  _timeDisplay(),
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(color: Colors.blueGrey, border: Border(top: BorderSide(color: Theme.of(context).dividerColor))),
+                padding: EdgeInsets.only(top: 3),
+                child: ListView.builder(
+                  itemCount: _history.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      color: Colors.white,
+                      child: ListTile(leading: Icon(Icons.alarm), title: Text(_formatDuration(_history[index]), textAlign: TextAlign.end,))
+                    );
+                  }
+                ),
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _toggleTracking,
+        tooltip: 'Toggle Tracking',
+        child: _tracking ? Icon(Icons.alarm_off) : Icon(Icons.alarm),
+        backgroundColor: _tracking ? Colors.red : Colors.blue,
+      ),
     );
   }
 }
